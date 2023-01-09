1b67134a907f72913119f3d186175d7b24d7c870
Rockesh Sam Manovah S W <rockesh@Rockeshs-MacBook-Air.local>
2023-01-09 11:54:46 +0530

    trim at end feature has been added

--- bulk_files_renamer/rename_file.py
+++ bulk_files_renamer/rename_file.py
@@ -1,9 +1,5 @@
 import os
-
-folder_path = input('Enter the path: ')
-#/Users/rockesh/Documents/songs
-file_list = os.listdir(folder_path)
-folder_exist = os.path.exists(folder_path)
+import re
 
 def trim_at_start_and_save(folderPath,fileName,numOfChars):
     if not fileName.startswith('.') and os.path.isfile(os.path.join(folderPath, fileName)):
@@ -11,22 +7,52 @@ def trim_at_start_and_save(folderPath,fileName,numOfChars):
         new_name_path = folderPath + '/' + new_name.strip()
         old_name_path = folderPath + '/' + fileName
         os.rename(old_name_path, new_name_path)
-        #print(old_name_path)
-        #print(new_name_path)
 
+def trim_at_end_and_save(folderPath,fileName,numOfChars):
+    if not fileName.startswith('.') and os.path.isfile(os.path.join(folderPath, fileName)):
+        cur_name = re.search('(.*)\..*',fileName)
+        cur_name = cur_name.group(1)
+        extension = re.search('.*\.(.*)',fileName)
+        extension = extension.group(1)
+        new_name = cur_name[:-numOfChars] + '.' + extension
+        new_name_path = folderPath + '/' + new_name.strip()
+        old_name_path = folderPath + '/' + fileName
+        os.rename(old_name_path, new_name_path)
 
-if ((len(file_list) > 0) and (folder_exist)):
-    option = input('[1] Trim from start\n[2] Trim at end\n[3] Append at start\n[4] Append at end\n\nEnter your selection: ')
-    if option == '1':
-        numberOfCharacters = input('How many characters you want to be removed from start: ')
-        numberOfCharacters = int(numberOfCharacters)
-        # print(file_list)
-        # print('----------\n----------\n----------')
-        for file in file_list:
-            #print(file)
-            # print(type(file))
-            # print(numberOfCharacters)
-            # print(type(numberOfCharacters))
-            trim_at_start_and_save(folder_path,file,numberOfCharacters)
+rename = True
+while rename == True:
+
+    option = input('[1] Trim from start\n[2] Trim at end\n[3] Append at start\n[4] Append at end\n[5] Exit\n\nEnter your selection: ')
+    if option in ['1','2','3','4']:
+        folder_path = input('Enter the path: ')
+        file_list = os.listdir(folder_path)
+        folder_exist = os.path.exists(folder_path)
+        if ((len(file_list) > 0) and (folder_exist)):
+            if option == '1':
+                numberOfCharacters = input('How many characters you want to be removed from start: ')
+                numberOfCharacters = int(numberOfCharacters)
+                for file in file_list:
+                    trim_at_start_and_save(folder_path,file,numberOfCharacters)
+            rename = True
+
+            if option == '2':
+                numberOfCharacters = input('How many characters you want to be removed from end: ')
+                numberOfCharacters = int(numberOfCharacters)
+                for file in file_list:
+                    trim_at_end_and_save(folder_path,file,numberOfCharacters)
+                rename = True
+
+            if option == '3':
+                print('This feature is under development... Wait for the next release')
+                rename = True
+
+            if option == '4':
+                print('This feature is under development... Wait for the next release')
+                rename = True
+
+    elif option == '5':
+        rename = False
+
     else:
-        print('Work in progress... Wait for the next release')
+        print('Kindly select correct option')
+        rename = True
